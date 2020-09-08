f1f2d532bf1b2ee4fd5ac14e282bb283e0fc5483
Julian <julianlen@gmail.com>
2020-09-08 15:56:40 -0300

    Forwarder test pass and also the token payment is tested

--- contracts/forwarder/test/TestForwarder.sol
+++ contracts/forwarder/test/TestForwarder.sol
@@ -9,21 +9,11 @@ contract TestForwarder {
     function callExecute(Forwarder forwarder, Forwarder.ForwardRequest memory req,
         bytes32 domainSeparator, bytes32 requestTypeHash, bytes memory suffixData, bytes memory sig) public payable {
         // solhint-disable-next-line avoid-low-level-calls
-        (bool success, bytes memory error) = req.tokenContract.call{gas: req.tokenGas}(
-        abi.encodeWithSelector(IERC20.transfer.selector, req.tokenRecipient, req.paybackTokens)
-        );
-
-        if (!success){
-            //re-throw the revert with the same revert reason.
-            GsnUtils.revertWithData(error);
-            return;
-        }
-        
-        (success,error) = forwarder.execute{value:msg.value}(req, domainSeparator, requestTypeHash, suffixData, sig);
-        emit Result(success, success ? "" : this.decodeErrorMessage(error));
+        (bool success, bytes memory error, uint256 lastTxSucc) = forwarder.execute{value:msg.value}(req, domainSeparator, requestTypeHash, suffixData, sig);
+        emit Result(success, success ? "" : this.decodeErrorMessage(error), lastTxSucc);
     }   
 
-    event Result(bool success, string error);
+    event Result(bool success, string error, uint256 lastTxSucc);
 
     function decodeErrorMessage(bytes calldata ret) external pure returns (string memory message) {
         //decode evert string: assume it has a standard Error(string) signature: simply skip the (selector,offset,length) fields
