4c7f497e21012899dffb37514d2861cb898f50cf
Julian <julianlen@gmail.com>
2020-11-27 10:43:10 -0300

    Tested GSNTestEnvironment with an erc20 token contract

--- test/GsnTestEnvironment.test.ts
+++ test/GsnTestEnvironment.test.ts
@@ -1,11 +1,12 @@
 import { GsnTestEnvironment, TestEnvironment } from '../src/relayclient/GsnTestEnvironment'
 import { HttpProvider } from 'web3-core'
 import { expectEvent } from '@openzeppelin/test-helpers'
-import { TestRecipientInstance, ProxyFactoryInstance } from '../types/truffle-contracts'
+import { TestRecipientInstance, ProxyFactoryInstance, TestTokenRecipientInstance } from '../types/truffle-contracts'
 import { getTestingEnvironment, createSmartWallet, getGaslessAccount } from './TestUtils'
 import { constants } from '../src/common/Constants'
 
 const TestRecipient = artifacts.require('TestRecipient')
+const TestTokenRecipient = artifacts.require('TestTokenRecipient')
 const ProxyFactory = artifacts.require('ProxyFactory')
 
 contract('GsnTestEnvironment', function () {
@@ -61,6 +62,38 @@ contract('GsnTestEnvironment', function () {
       assert.equal(events[0].event, 'SampleRecipientEmitted')
       assert.equal(events[0].returnValues.msgSender.toLocaleLowerCase(), wallet.address.toLocaleLowerCase())
     })
+
+    it('should relay using relayTransaction invoking an ERC20 contract', async () => {
+      const tokenReceiverAddress = '0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826'
+      const sender = getGaslessAccount()
+      const proxyFactory: ProxyFactoryInstance = await ProxyFactory.at(testEnvironment.deploymentResult.factoryAddress)
+      const str: TestTokenRecipientInstance = await TestTokenRecipient.new()
+
+      const wallet = await createSmartWallet(sender.address, proxyFactory, sender.privateKey, (await getTestingEnvironment()).chainId)
+      await str.mint('200', wallet.address)
+      testEnvironment.relayProvider.relayClient.accountManager.addAccount(sender)
+
+      const ret = await testEnvironment.relayProvider.relayClient.relayTransaction({
+        from: sender.address,
+        to: str.address,
+        forwarder: wallet.address,
+        paymaster: testEnvironment.deploymentResult.naivePaymasterAddress,
+        paymasterData: '0x',
+        gas: '0x' + 1e6.toString(16),
+        data: str.contract.methods.transfer(tokenReceiverAddress, '5').encodeABI(),
+        tokenRecipient: constants.ZERO_ADDRESS,
+        tokenAmount: '0x00',
+        tokenContract: constants.ZERO_ADDRESS,
+        factory: constants.ZERO_ADDRESS,
+        clientId: '1'
+      })
+
+      assert.deepEqual([...ret.relayingErrors.values(), ...ret.pingErrors.values()], [])
+      const events = await str.contract.getPastEvents()
+      assert.equal(events[0].event, 'Transfer')
+      const balance = await str.balanceOf(tokenReceiverAddress)
+      assert.equal(balance.toString(), '5')
+    })
   })
 
   describe('using RelayProvider', () => {
@@ -93,6 +126,30 @@ contract('GsnTestEnvironment', function () {
       }
       const ret = await sr.emitMessage('hello', txDetails)
       expectEvent(ret, 'SampleRecipientEmitted', { msgSender: wallet.address })
+    })
+
+    it('should send relayed transaction invoking an ERC20 contract through RelayProvider', async () => {
+      const tokenReceiverAddress = '0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826'
+      const sender = getGaslessAccount()
+      const proxyFactory: ProxyFactoryInstance = await ProxyFactory.at(testEnvironment.deploymentResult.factoryAddress)
+      const str: TestTokenRecipientInstance = await TestTokenRecipient.new()
+
+      const wallet = await createSmartWallet(sender.address, proxyFactory, sender.privateKey, (await getTestingEnvironment()).chainId)
+      testEnvironment.relayProvider.addAccount(sender)
+      await str.mint('200', wallet.address)
+
+      // @ts-ignore
+      TestTokenRecipient.web3.setProvider(testEnvironment.relayProvider)
+
+      const txDetails = {
+        from: sender.address,
+        paymaster: testEnvironment.deploymentResult.naivePaymasterAddress,
+        forwarder: wallet.address
+      }
+      const ret = await str.transfer(tokenReceiverAddress, '5', txDetails)
+      expectEvent(ret, 'Transfer')
+      const balance = await str.balanceOf(tokenReceiverAddress)
+      assert.equal(balance.toString(), '5')
     })
   })
 })
