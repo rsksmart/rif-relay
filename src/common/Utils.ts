2cf818397d30546fa01dfc8a2d9a3fb6cb054865
Diego Masini <diego.masini@iovlabs.org>
2021-01-18 16:08:42 -0300

    Removed unused imports

--- src/common/Utils.ts
+++ src/common/Utils.ts
@@ -1,7 +1,6 @@
-import BN from 'bn.js'
 import abi from 'web3-eth-abi'
 import ethUtils from 'ethereumjs-util'
-import web3Utils, { toWei } from 'web3-utils'
+import web3Utils from 'web3-utils'
 import { EventData } from 'web3-eth-contract'
 import { JsonRpcResponse } from 'web3-core-helpers'
 import { PrefixedHexString } from 'ethereumjs-tx'
