e68b2dab8c6cdd1b26ba18ba7b4ac60db8f9e11a
Diego Masini <diego.masini@iovlabs.org>
2021-01-29 14:38:11 -0300

    Added return statement to isRelayManagerStaked function in RelayHub contract

--- contracts/RelayHub.sol
+++ contracts/RelayHub.sol
@@ -181,7 +181,7 @@ contract RelayHub is IRelayHub {
         
         //If no revert, then return true
         require(succ, "relay manager not staked");
-       
+        return true;
     }
 
     modifier penalizerOnly () {
