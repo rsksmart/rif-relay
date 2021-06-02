9bc6296f20973c222c442fafaf30d5cdf6739d5f
Julian <julianlen@gmail.com>
2021-06-02 10:38:58 -0300

    extract_abi modified

--- scripts/extract_abi.js
+++ scripts/extract_abi.js
@@ -10,12 +10,11 @@ const outAbiFolder = 'src/common'
 const contractsFolderToExtract = './contracts/interfaces'
 
 const files = fs.readdirSync(contractsFolderToExtract)
-files.push('IForwarder.sol')
-files.push('IWalletFactory.sol')
+// files.push('IForwarder.sol')
+// files.push('IWalletFactory.sol')
 
 files.forEach(file => {
   const c = 'interfaces/' + file.replace(/.sol/, '')
-
   const outNodeFile = outAbiFolder + '/' + c + '.json'
   const jsonFile = './artifacts/contracts/'+ c + '.sol/' +`${c.replace(/interfaces./, '')}.json`
   const abiStr = JSON.parse(fs.readFileSync(jsonFile, { encoding: 'ascii' }))
