b8ce54e98622907a55d283edb39f2573631e8ae6
Julian <julianlen@gmail.com>
2021-06-03 13:54:30 -0300

    Enveloping utils  adapted to empty get code

--- test/EnvelopingUtils.test.ts
+++ test/EnvelopingUtils.test.ts
@@ -152,7 +152,7 @@ contract('Enveloping utils', function (accounts) {
 
     it('Should deploy a smart wallet correctly and relay a tx using enveloping utils without tokens', async () => {
       const expectedInitialCode = await web3.eth.getCode(swAddress)
-      assert.equal('0x00', expectedInitialCode)
+      assert.equal('0x', expectedInitialCode)
 
       const txDeployHash = await deploySmartWallet(constants.ZERO_ADDRESS, '0', '0')
 
@@ -187,7 +187,7 @@ contract('Enveloping utils', function (accounts) {
     it('Should deploy a smart wallet correctly and relay a tx using enveloping utils paying with tokens', async () => {
       const expectedInitialCode = await web3.eth.getCode(swAddress)
       const balanceTransfered = new BN(10)
-      assert.equal('0x00', expectedInitialCode)
+      assert.equal('0x', expectedInitialCode)
       await tokenContract.mint('100', swAddress)
       const previousBalance = await tokenContract.balanceOf(workerAddress)
 
