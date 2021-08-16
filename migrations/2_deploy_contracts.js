570a83678a3d1c0514458e4ec2e994dbff0b91f3
mortelli <ortellimarcelo@gmail.com>
2021-08-16 10:45:04 -0300

    apply linter fix

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -20,7 +20,7 @@ module.exports = async function (deployer) {
   await deployer.deploy(RelayHub, Penalizer.address, 4, 4, 4, 4)
 
   const penalizer = await new web3.eth.Contract(Penalizer.abi, Penalizer.address)
-  await penalizer.methods.setHub(RelayHub.address).send({from: await penalizer.methods.owner().call()})
+  await penalizer.methods.setHub(RelayHub.address).send({ from: await penalizer.methods.owner().call() })
 
   await deployer.deploy(SmartWallet)
   await deployer.deploy(SmartWalletFactory, SmartWallet.address)
