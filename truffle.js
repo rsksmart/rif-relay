3cac7b86f884d762ce71e1ba572fa8aeeb80a470
David Ugbero <david.ugbero@iovlabs.org>
2022-07-18 09:21:28 +0100

    try with node v14

--- truffle.js
+++ truffle.js
@@ -29,6 +29,14 @@ module.exports = {
             gas: 6300000,
             gasPrice: 60000000 // 0.06 gwei
         },
+        regtest: {
+            verbose: process.env.VERBOSE,
+            host: 'enveloping-rskj',
+            port: 4444,
+            network_id: 33,
+            gas: 6300000,
+            gasPrice: 60000000 // 0.06 gwei
+        },
         testnet: {
             provider: function () {
                 return new HDWalletProvider(
