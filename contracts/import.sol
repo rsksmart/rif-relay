29fe24d8de855bdf1c8dfd7315b9e57745795eca
Antonio Morrone <antonio@iovlabs.org>
2023-01-10 14:35:52 +0100

    fix: use the contracts from the root folder instead of dist

--- contracts/import.sol
+++ contracts/import.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier:MIT
 pragma solidity ^0.6.12;
 
-import '@rsksmart/rif-relay-contracts/dist/contracts/smartwallet/SmartWallet.sol';
-import '@rsksmart/rif-relay-contracts/dist/contracts/factory/SmartWalletFactory.sol';
+import '@rsksmart/rif-relay-contracts/contracts/smartwallet/SmartWallet.sol';
+import '@rsksmart/rif-relay-contracts/contracts/factory/SmartWalletFactory.sol';
