90c20ff1087761f27eff9ca707ae4f0e08037215
Antonio Morrone <antonio@iovlabs.org>
2023-01-10 15:50:29 +0100

    PP-358: configure the project to use ethers (#231)
    * chore: update package version
    
    * feat: replace truffle/web3 with hardhat
    
    * chore: add clean script in package.json
    
    * test: fix hardhat network
    
    * fix: remove unnecessary peer dependency
    
    * fix: use the contracts from the root folder instead of dist
    
    * chore: rename prettierrc to prettierrc.json5 for consistency across all the project
    
    * chore: update package-lock.json
    
    * chore: update rif-relay-contracts dependency

---
+++ hardhat.config.ts
@@ -0,0 +1,70 @@
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import { HttpNetworkUserConfig } from 'hardhat/types';
+import '@nomiclabs/hardhat-ethers';
+
+const DEFAULT_MNEMONIC =
+  'stuff slice staff easily soup parent arm payment cotton trade scatter struggle';
+const { PK, MNEMONIC } = process.env;
+const sharedNetworkConfig: HttpNetworkUserConfig = {};
+if (PK) {
+  sharedNetworkConfig.accounts = [PK];
+} else {
+  sharedNetworkConfig.accounts = {
+    mnemonic: MNEMONIC || DEFAULT_MNEMONIC,
+  };
+}
+
+const config: HardhatUserConfig = {
+  // paths: {
+  //   sources: './node_modules/@rsksmart/rif-relay-contracts/contracts'
+  // },
+  solidity: {
+    compilers: [
+      {
+        version: '0.6.12',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+          outputSelection: {
+            '*': {
+              '*': ['storageLayout'],
+            },
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    regtest_ci: {
+      url: 'http://enveloping-rskj:4444',
+      chainId: 33,
+    },
+    regtest: {
+      url: 'http://localhost:4444',
+      chainId: 33,
+    },
+    testnet: {
+      ...sharedNetworkConfig,
+      url: 'https://public-node.testnet.rsk.co',
+      chainId: 31,
+    },
+  },
+  typechain: {
+    target: 'ethers-v5',
+    outDir: 'typechain-types',
+  },
+  /* contractSizer: {
+    alphaSort: true,
+    disambiguatePaths: false,
+    runOnCompile: true,
+    strict: false,
+  }*/
+  mocha: {
+    timeout: 20000,
+  },
+};
+
+export default config;
