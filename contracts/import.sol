671a6c16a9468fdcdaead40eff1f3325ab303572
Antonio Morrone <antonio@iovlabs.org>
2023-01-10 15:50:29 +0100

    PP-358: configure the project to use ethers (#231)
    * chore: update package version
    
    * feat: replace truffle/web3 with hardhat
    
    * chore: add clean script in package.json
    
    * test: fix hardhat network
    
    * fix: remove unnecessary peer dependency
    
    * fix: use the contracts from the root folder instead of dist
    
    * chore: rename prettierrc to prettierrc.json5 for consistency across all the project
    
    * chore: update package-lock.json
    
    * chore: update rif-relay-contracts dependency

---
+++ contracts/import.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.6.12;
+
+import '@rsksmart/rif-relay-contracts/contracts/smartwallet/SmartWallet.sol';
+import '@rsksmart/rif-relay-contracts/contracts/factory/SmartWalletFactory.sol';
