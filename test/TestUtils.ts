3de50d72c855e97e744fdcbd4d9f7d1b8493ea97
Francisco Tobar <francisco.tobar@iovlabs.org>
2023-01-16 20:48:38 -0600

    test: refactor

---
+++ test/TestUtils.ts
@@ -0,0 +1,394 @@
+import childProcess, { ChildProcessWithoutNullStreams } from 'child_process';
+import fs from 'fs';
+import path from 'path';
+import { BigNumberish, constants, utils, Wallet } from 'ethers';
+import { expect } from 'chai';
+import config from 'config';
+
+import {
+  RelayHub,
+  SmartWalletFactory,
+  CustomSmartWalletFactory,
+  IForwarder,
+} from '@rsksmart/rif-relay-contracts';
+import {
+  defaultEnvironment,
+  RelayHubConfiguration,
+  ServerConfigParams,
+  sleep,
+} from '@rsksmart/rif-relay-server';
+import {
+  DeployRequest,
+  deployRequestType,
+  EnvelopingRequest,
+  getEnvelopingRequestDataV4Field,
+  HttpClient,
+  HttpWrapper,
+  HubInfo,
+  isDeployRequest,
+  RelayRequest,
+  relayRequestType,
+} from '@rsksmart/rif-relay-client';
+import { ethers as hardhat } from 'hardhat';
+import { _TypedDataEncoder } from 'ethers/lib/utils';
+
+const SERVER_WORK_DIR = '/tmp/enveloping/test/server';
+const ONE_FIELD_IN_BYTES = 32;
+
+type StartRelayParams = {
+  serverConfig: ServerConfigParams;
+  delay?: number;
+  stake?: BigNumberish;
+  relayOwner: string;
+};
+
+const provider = hardhat.provider;
+
+const startRelay = async (relayHub: RelayHub, options: StartRelayParams) => {
+  const { serverConfig, delay, stake, relayOwner } = options;
+
+  fs.rmSync(SERVER_WORK_DIR, { recursive: true, force: true });
+
+  const originalConfig = { ...config };
+
+  config.util.extendDeep(originalConfig, {
+    ...serverConfig,
+    contracts: {
+      relayHubAddress: relayHub.address,
+    },
+    app: {
+      workdir: SERVER_WORK_DIR,
+      devMode: true,
+      checkInterval: 10,
+      logLevel: 5,
+    },
+  });
+
+  const url = config.get<string>('app.url');
+
+  const runServerPath = path.resolve(
+    __dirname,
+    '../node_modules/@rsksmart/rif-relay-server/dist/commands/Start.js'
+  );
+
+  const proc: ChildProcessWithoutNullStreams & { alreadyStarted?: number } =
+    childProcess.spawn('node', [runServerPath]);
+
+  const { relayManagerAddress } = await verifyRelayServerStatus(url, 3);
+
+  console.log('Relay Server Manager Address', relayManagerAddress);
+
+  await provider.getSigner(relayOwner).sendTransaction({
+    to: relayManagerAddress,
+    value: utils.parseEther('2'),
+  });
+
+  await relayHub.stakeForAddress(relayManagerAddress, delay || 2000, {
+    from: relayOwner,
+    value: stake || utils.parseEther('1'),
+  });
+
+  const { ready, relayWorkerAddress } = await verifyRelayServerStatus(
+    url,
+    25,
+    500,
+    true
+  );
+
+  expect(ready, 'Timed out waiting for relay to get staked and registered').is
+    .ok;
+
+  return {
+    proc,
+    worker: relayWorkerAddress,
+    manager: relayManagerAddress,
+  };
+};
+
+const stopRelay = (proc: ChildProcessWithoutNullStreams): void => {
+  proc.kill();
+};
+
+const evmMine = async (): Promise<unknown> => {
+  return provider.send('evm_mine', []);
+};
+
+const evmMineMany = async (count: number): Promise<void> => {
+  for (let i = 0; i < count; i++) {
+    await evmMine();
+  }
+};
+
+const increaseBlockchainTime = async (time: number): Promise<void> => {
+  await provider.send('evm_increaseTime', [time]);
+  await evmMine();
+};
+
+const createSnapshot = async (): Promise<string> => {
+  return (await provider.send('evm_snapshot', [])) as string;
+};
+
+const revertSnapshot = async (snapshotId: string) => {
+  return (await provider.send('evm_revert', [snapshotId])) as boolean;
+};
+
+const verifyRelayServerStatus = async (
+  url: string,
+  attempts: number,
+  interval = 1000,
+  isReady = false
+): Promise<HubInfo> => {
+  let response: HubInfo | undefined;
+  const httpClient = new HttpClient(new HttpWrapper(undefined, 'silent'));
+
+  for (let i = 0; i < attempts; i++) {
+    try {
+      console.log('sleep before cont.');
+      await sleep(interval);
+      response = await httpClient.getChainInfo(url);
+
+      if (isReady && !response.ready) {
+        continue;
+      }
+
+      return response;
+    } catch (e) {
+      /* empty */
+    }
+  }
+
+  expect(response, "can't ping server").to.be.ok;
+
+  throw Error("can't ping server");
+};
+
+// An existing account in RSKJ that have been depleted
+const getExistingGaslessAccount = async (): Promise<Wallet> => {
+  const gaslessAccount = new Wallet(
+    '0x082f57b8084286a079aeb9f2d0e17e565ced44a2cb9ce4844e6d4b9d89f3f595',
+    provider
+  );
+
+  const balance = await gaslessAccount.getBalance();
+  if (!balance.gte(0)) {
+    await gaslessAccount.sendTransaction({
+      to: constants.AddressZero,
+      gasPrice: 1,
+      gasLimit: 21000,
+      value: balance.sub(21000),
+    });
+  }
+
+  await expect(
+    gaslessAccount.getBalance(),
+    'Gassless account should have no funds'
+  ).to.be.eventually.eql(constants.Zero);
+
+  return gaslessAccount;
+};
+
+const deployRelayHub = async (
+  penalizer: string = constants.AddressZero,
+  configOverride: Partial<RelayHubConfiguration>
+): Promise<RelayHub> => {
+  const relayHubConfiguration: RelayHubConfiguration = {
+    ...defaultEnvironment!.relayHubConfiguration,
+    ...configOverride,
+  };
+
+  const relayHubFactory = await hardhat.getContractFactory('RelayHub');
+
+  const {
+    maxWorkerCount,
+    minimumUnstakeDelay,
+    minimumStake,
+    minimumEntryDepositValue,
+  } = relayHubConfiguration;
+
+  return relayHubFactory.deploy(
+    penalizer,
+    maxWorkerCount,
+    minimumUnstakeDelay,
+    minimumStake,
+    minimumEntryDepositValue
+  );
+};
+
+const createSmartWalletFactory = async (
+  template: IForwarder,
+  isCustom = false
+) => {
+  const factory = isCustom
+    ? await hardhat.getContractFactory('CustomSmartWalletFactory')
+    : await hardhat.getContractFactory('SmartWalletFactory');
+
+  return factory.deploy(template.address);
+};
+
+const createSmartWallet = async (
+  relayHub: string,
+  owner: string,
+  factory: SmartWalletFactory | CustomSmartWalletFactory,
+  wallet: Wallet,
+  tokenContract = constants.AddressZero,
+  tokenAmount = 0,
+  tokenGas = 0,
+  recoverer = constants.AddressZero,
+  index: 0,
+  validUntilTime: 0,
+  logicAddr = constants.AddressZero,
+  initParams = '0x00'
+) => {
+  const envelopingRequest: DeployRequest = {
+    request: {
+      relayHub,
+      from: owner,
+      to: logicAddr,
+      value: 0,
+      nonce: 0,
+      data: initParams,
+      tokenContract,
+      tokenAmount,
+      tokenGas,
+      recoverer,
+      index,
+      validUntilTime,
+    },
+    relayData: {
+      gasPrice: 1,
+      feesReceiver: constants.AddressZero,
+      callForwarder: factory.address,
+      callVerifier: constants.AddressZero,
+    },
+  };
+
+  const { signature, suffixData } = await signEnvelopingRequest(
+    envelopingRequest,
+    wallet
+  );
+
+  const transaction = await factory.relayedUserSmartWalletCreation(
+    envelopingRequest.request,
+    suffixData,
+    constants.AddressZero,
+    signature
+  );
+
+  const receipt = await transaction.wait();
+
+  console.log('Cost of deploying SmartWallet: ', receipt.cumulativeGasUsed);
+
+  const isCustom = !!logicAddr && logicAddr !== constants.AddressZero;
+
+  const swAddress = isCustom
+    ? await (factory as CustomSmartWalletFactory).getSmartWalletAddress(
+        owner,
+        recoverer,
+        logicAddr,
+        initParams,
+        index
+      )
+    : await (factory as SmartWalletFactory).getSmartWalletAddress(
+        owner,
+        recoverer,
+        index
+      );
+
+  return isCustom
+    ? hardhat.getContractAt('CustomSmartWallet', swAddress)
+    : hardhat.getContractAt('SmartWallet', swAddress);
+};
+
+const prepareRelayTransaction = async (
+  relayHub: string,
+  owner: string,
+  wallet: Wallet,
+  tokenContract = constants.AddressZero,
+  tokenAmount = 0,
+  tokenGas = 0,
+  validUntilTime: 0,
+  logicAddr = constants.AddressZero,
+  initParams = '0x00',
+  gas = 0,
+  swAddress: string
+) => {
+  const envelopingRequest: RelayRequest = {
+    request: {
+      relayHub,
+      from: owner,
+      to: logicAddr,
+      value: 0,
+      nonce: 0,
+      data: initParams,
+      tokenContract,
+      tokenAmount,
+      tokenGas,
+      validUntilTime,
+      gas,
+    },
+    relayData: {
+      gasPrice: 1,
+      feesReceiver: constants.AddressZero,
+      callForwarder: swAddress,
+      callVerifier: constants.AddressZero,
+    },
+  };
+
+  const { signature } = await signEnvelopingRequest(envelopingRequest, wallet);
+
+  return {
+    envelopingRequest,
+    signature,
+  };
+};
+
+const signEnvelopingRequest = async (
+  envelopingRequest: EnvelopingRequest,
+  wallet: Wallet
+) => {
+  const {
+    relayData: { callForwarder },
+  } = envelopingRequest;
+
+  const isDeploy = isDeployRequest(envelopingRequest);
+
+  const { chainId } = await provider.getNetwork();
+
+  const data = getEnvelopingRequestDataV4Field({
+    chainId,
+    verifier: callForwarder.toString(),
+    requestTypes: isDeploy ? deployRequestType : relayRequestType,
+    envelopingRequest,
+  });
+
+  const { domain, types, value } = data;
+
+  const signature = await wallet._signTypedData(domain, types, value);
+
+  const messageSize = Object.keys(value).length;
+
+  const enconded = _TypedDataEncoder.encode(domain, types, value);
+
+  const suffixData = enconded.slice((1 + messageSize) * ONE_FIELD_IN_BYTES);
+
+  return {
+    signature,
+    suffixData,
+  };
+};
+
+export {
+  startRelay,
+  stopRelay,
+  evmMine,
+  evmMineMany,
+  increaseBlockchainTime,
+  createSnapshot,
+  revertSnapshot,
+  getExistingGaslessAccount,
+  createSmartWalletFactory,
+  createSmartWallet,
+  prepareRelayTransaction,
+  signEnvelopingRequest,
+  deployRelayHub,
+};
---
+++ test/relayserver/ServerTestEnvironments.ts
@@ -0,0 +1,233 @@
+import {
+  KeyManager,
+  RelayServer,
+  ServerDependencies,
+  TxStoreManager,
+} from '@rsksmart/rif-relay-server';
+import {
+  EnvelopingRequest,
+  EnvelopingTxRequest,
+  HubInfo,
+  RelayClient,
+  UserDefinedEnvelopingRequest,
+} from '@rsksmart/rif-relay-client';
+import { utils, Wallet } from 'ethers';
+import {
+  assertEventHub,
+  getTemporaryWorkdirs,
+  ServerWorkdirs,
+} from './ServerTestUtils';
+import {
+  IForwarder,
+  RelayHubInterface,
+  RelayHub__factory,
+} from '@rsksmart/rif-relay-contracts';
+import { ethers as hardhat } from 'hardhat';
+import { expect } from 'chai';
+import { deployRelayHub, createSmartWalletFactory } from '../TestUtils';
+import config from 'config';
+
+const provider = hardhat.provider;
+const dayInSec = 24 * 60 * 60;
+const weekInSec = dayInSec * 7;
+
+const newServerInstance = async (
+  relayOwner: Wallet,
+  template: IForwarder,
+  serverWorkdirs?: ServerWorkdirs
+) => {
+  const relayServer = await newServerInstanceNoInit(
+    relayOwner,
+    template,
+    serverWorkdirs
+  );
+  await relayServer.init();
+  const latestBlock = await provider.getBlock('latest');
+  const receipts = await relayServer._worker(latestBlock.number);
+  await assertEventHub('RelayServerRegistered', receipts, ''); // sanity check
+  await relayServer._worker(latestBlock.number + 1);
+
+  return relayServer;
+};
+
+const newServerInstanceNoInit = async (
+  relayOwner: Wallet,
+  template: IForwarder,
+  serverWorkdirs?: ServerWorkdirs,
+  unstakeDelay = weekInSec
+) => {
+  const relayServer = await newServerInstanceNoFunding(
+    template,
+    serverWorkdirs
+  );
+
+  const { relayHubAddress, relayManagerAddress } = relayServer.pingHandler();
+
+  await relayOwner.sendTransaction({
+    value: utils.parseEther('2'),
+  });
+
+  const relayHub = RelayHub__factory.connect(relayHubAddress, provider);
+
+  await relayHub
+    .connect(relayOwner)
+    .stakeForAddress(relayManagerAddress, unstakeDelay, {
+      value: utils.parseEther('1'),
+    });
+
+  return relayServer;
+};
+
+const newServerInstanceNoFunding = async (
+  template: IForwarder,
+  serverWorkdirs?: ServerWorkdirs
+) => {
+  const { workdir } = getTemporaryWorkdirs();
+
+  const managerKeyManager = createKeyManager(serverWorkdirs?.managerWorkdir);
+  const workersKeyManager = createKeyManager(serverWorkdirs?.workersWorkdir);
+  const txStoreManager = new TxStoreManager({
+    workdir: serverWorkdirs?.workdir ?? workdir,
+  });
+
+  const dependencies: ServerDependencies = {
+    txStoreManager,
+    managerKeyManager,
+    workersKeyManager,
+  };
+
+  const factory = await createSmartWalletFactory(template);
+  const relayHub = await deployRelayHub(undefined, {});
+  const deployVerifierFactory = await hardhat.getContractFactory(
+    'DeployVerifier'
+  );
+  const deployVerifier = await deployVerifierFactory.deploy(factory.address);
+  const relayVerifierFactory = await hardhat.getContractFactory(
+    'RelayVerifier'
+  );
+  const relayVerifier = await relayVerifierFactory.deploy(factory.address);
+
+  const originalConfig = { ...config };
+
+  config.util.extendDeep(originalConfig, {
+    contracts: {
+      relayHubAddress: relayHub.address,
+      deployVerifierAddress: deployVerifier.address,
+      relayVerifierAddress: relayVerifier.address,
+      smartWalletFactoryAddress: factory.address,
+    },
+    app: {
+      devMode: true,
+      checkInterval: 10,
+      logLevel: 5,
+    },
+  });
+
+  const relayServer = new RelayServer(dependencies);
+
+  return relayServer;
+};
+
+const createKeyManager = (workdir?: string): KeyManager => {
+  if (workdir != null) {
+    return new KeyManager(1, workdir);
+  } else {
+    return new KeyManager(1, undefined, utils.randomBytes(32).toString());
+  }
+};
+
+const createRelayHttpRequest = async (
+  envelopingRequest: UserDefinedEnvelopingRequest,
+  relayClient: RelayClient,
+  hubInfo: HubInfo
+): Promise<EnvelopingTxRequest> => {
+  type RelayClientExposed = {
+    _prepareHttpRequest: (
+      hubInfo: HubInfo,
+      envelopingRequest: EnvelopingRequest
+    ) => Promise<EnvelopingTxRequest>;
+    _getEnvelopingRequestDetails: (
+      envelopingRequest: UserDefinedEnvelopingRequest
+    ) => Promise<EnvelopingRequest>;
+  };
+
+  const localClient = relayClient as unknown as RelayClientExposed;
+
+  const envelopingRequestDetails =
+    await localClient._getEnvelopingRequestDetails(envelopingRequest);
+
+  return await localClient._prepareHttpRequest(
+    hubInfo,
+    envelopingRequestDetails
+  );
+};
+
+const relayTransaction = async (
+  envelopingRequest: UserDefinedEnvelopingRequest,
+  relayServer: RelayServer,
+  relayClient: RelayClient,
+  assertRelayed = true
+) => {
+  const hubInfo = relayServer.pingHandler();
+  const envelopingTx = await createRelayHttpRequest(
+    envelopingRequest,
+    relayClient,
+    hubInfo
+  );
+  const relayTransaction = await relayServer.createRelayTransaction(
+    envelopingTx
+  );
+
+  if (assertRelayed) {
+    await assertTransactionRelayed(
+      hubInfo,
+      relayTransaction.txHash,
+      utils.keccak256(envelopingTx.metadata.signature)
+    );
+  }
+
+  return relayTransaction;
+};
+
+const assertTransactionRelayed = async (
+  hubInfo: HubInfo,
+  txHash: string,
+  hashSignature: string
+) => {
+  const receipt = await provider.getTransactionReceipt(txHash);
+
+  if (!receipt) {
+    throw new Error('Transaction Receipt not found');
+  }
+
+  const relayHubInterface: RelayHubInterface =
+    RelayHub__factory.createInterface();
+
+  const decodedLogs = receipt.logs.map((log) =>
+    relayHubInterface.parseLog(log)
+  );
+  const event = decodedLogs.find((e) => e.name === 'TransactionRelayed');
+
+  if (!event) {
+    throw new Error(
+      'TransactionRelayed not found, maybe transaction was not relayed successfully'
+    );
+  }
+
+  const { relayManagerAddress, relayWorkerAddress } = hubInfo;
+
+  expect(event.args['relayManager']).to.be.equal(relayManagerAddress);
+
+  expect(event.args['relayWorker']).to.be.equal(relayWorkerAddress);
+
+  expect(event.args['relayRequestSigHash']).to.be.equal(hashSignature);
+};
+
+export {
+  newServerInstance,
+  newServerInstanceNoInit,
+  newServerInstanceNoFunding,
+  createRelayHttpRequest,
+  relayTransaction,
+  assertTransactionRelayed,
+};
