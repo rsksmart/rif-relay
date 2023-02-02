fb6198be4396e9c1ea738cf08a2f8cbae44ca2e9
AndresQuijano <66392159+AndresQuijano@users.noreply.github.com>
2023-02-02 08:09:08 -0500

    PP639: Migrate SmartWallet tests (#232)
    * feat: migrate the RIFSmartWallet integration tests
    
    * refactor: implement @metamask signatures
    
    * fix: change the way we create smart wallets
    use ethers provider instead of hardhat
    rename files
    
    ---------
    
    Co-authored-by: Antonio Morrone <antonio@iovlabs.org>

---
+++ contracts/TestTarget.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.6.12;
+
+import "hardhat/console.sol";
+
+contract TestTarget {
+
+    string public  versionRecipient = "2.0.1+enveloping.test.recipient";
+
+
+    // solhint-disable-next-line no-empty-blocks
+    receive() external payable {}
+
+    event TestForwarderMessage(string message, address msgSender, address origin);
+
+    function emitMessage(string memory message) public {
+
+        // solhint-disable-next-line avoid-tx-origin
+        emit TestForwarderMessage(message, msg.sender, tx.origin);
+    }
+
+
+    function mustReceiveEth(uint256 sentValue) public payable {
+        require( msg.value == sentValue, "Not enough balance");
+    }
+
+    event Reverting(string message);
+
+    function testRevert() public {
+        require(address(this) == address(0), "always fail");
+        emit Reverting("if you see this revert failed...");
+    }
+}
