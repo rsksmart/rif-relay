e1f932ed56f7337d6927d72af5e1d4a1f9194492
Christos Otarola <otarola.christos@gmail.com>
2023-03-22 06:37:08 -0400

    PP-635/relay-client-tests (#246)
    * test(RelayClient): migration of the existing integration tests
    * refactor: use already defined deployContract function
    
    ---------
    
    Co-authored-by: Francisco Tobar <francisco.tobar@iovlabs.org>
    Co-authored-by: Antonio Morrone <antonio@iovlabs.org>

--- test/utils/TestUtils.ts
+++ test/utils/TestUtils.ts
@@ -1,6 +1,3 @@
-import childProcess, { ChildProcessWithoutNullStreams } from 'child_process';
-import fs from 'fs';
-import path from 'path';
 import { BigNumberish, constants, Contract, utils, Wallet } from 'ethers';
 import chaiAsPromised from 'chai-as-promised';
 import { expect, use } from 'chai';
@@ -9,16 +6,12 @@ import {
   SmartWalletFactory,
   CustomSmartWalletFactory,
   IForwarder,
-  RelayHub__factory,
   SmartWallet,
   CustomSmartWalletDeployVerifier,
 } from '@rsksmart/rif-relay-contracts';
 import {
   defaultEnvironment,
-  getServerConfig,
   RelayHubConfiguration,
-  ServerConfigParams,
-  sleep,
 } from '@rsksmart/rif-relay-server';
 import {
   DeployRequestBody,
@@ -26,9 +19,6 @@ import {
   EnvelopingRequest,
   EnvelopingRequestData,
   getEnvelopingRequestDataV4Field,
-  HttpClient,
-  HttpWrapper,
-  HubInfo,
   isDeployRequest,
   RelayRequest,
   RelayRequestBody,
@@ -57,8 +47,6 @@ import CustomSmartWalletJson from '../../artifacts/@rsksmart/rif-relay-contracts
 
 use(chaiAsPromised);
 
-const ATTEMPTS_GET_SERVER_STATUS = 3;
-const ATTEMPTS_GET_SERVER_READY = 25;
 const ONE_FIELD_IN_BYTES = 32;
 const CHARS_PER_FIELD = 64;
 const PREFIX_HEX = '0x';
@@ -67,14 +55,6 @@ type SupportedSmartWallet = CustomSmartWallet | SmartWallet;
 type SupportedSmartWalletFactory =
   | CustomSmartWalletFactory
   | SmartWalletFactory;
-
-type StartRelayParams = {
-  serverConfig: ServerConfigParams;
-  delay?: number;
-  stake?: BigNumberish;
-  relayOwner: string;
-  relayHubAddress: string;
-};
 
 type CreateSmartWalletParams = {
   relayHub: string;
@@ -114,101 +94,6 @@ const RSK_URL = nodeConfig.get<string>(
 );
 
 const { provider } = ethers;
-
-const startRelay = async (options: StartRelayParams) => {
-  const { delay, stake, relayOwner } = options;
-
-  const {
-    app: { workdir, url },
-  } = getServerConfig();
-
-  fs.rmSync(workdir, { recursive: true, force: true });
-
-  const runServerPath = path.resolve(
-    __dirname,
-    '../node_modules/@rsksmart/rif-relay-server/dist/commands/Start.js'
-  );
-
-  const proc: ChildProcessWithoutNullStreams & { alreadyStarted?: number } =
-    childProcess.spawn('node', [runServerPath]);
-
-  const client = new HttpClient(new HttpWrapper(undefined, 'silent'));
-
-  const { relayManagerAddress, relayHubAddress } = await doUntilDefined(
-    () => client.getChainInfo(url),
-    ATTEMPTS_GET_SERVER_STATUS
-  );
-
-  console.log('Relay Server Manager Address', relayManagerAddress);
-
-  await provider.getSigner(relayOwner).sendTransaction({
-    to: relayManagerAddress,
-    value: utils.parseEther('2'),
-  });
-
-  const relayHub = RelayHub__factory.connect(relayHubAddress, provider);
-
-  await relayHub.stakeForAddress(relayManagerAddress, delay || 2000, {
-    from: relayOwner,
-    value: stake || utils.parseEther('1'),
-  });
-
-  const { relayWorkerAddress } = await doUntilDefined(
-    () => getServerReady(url, client),
-    ATTEMPTS_GET_SERVER_READY,
-    'Server is not ready to relay transactions'
-  );
-
-  return {
-    proc,
-    worker: relayWorkerAddress,
-    manager: relayManagerAddress,
-  };
-};
-
-const getServerReady = async (
-  url: string,
-  client = new HttpClient(new HttpWrapper(undefined, 'silent'))
-): Promise<HubInfo | undefined> => {
-  const response = await client.getChainInfo(url);
-
-  if (response.ready) {
-    return response;
-  }
-
-  return undefined;
-};
-
-const doUntilDefined = async (
-  functionToWait: () => Promise<HubInfo | undefined>,
-  attempts: number,
-  customErrorMessage?: string,
-  interval = 1000
-): Promise<HubInfo> => {
-  for (let i = 0; i < attempts; i++) {
-    try {
-      console.log('sleep before cont.');
-      await sleep(interval);
-      const response = await functionToWait();
-
-      if (!response) {
-        continue;
-      }
-
-      return response;
-    } catch (e) {
-      console.log('getChainInfo failed');
-    }
-  }
-
-  throw Error(
-    `HubInfo poll expired: (${customErrorMessage ?? 'Server is not reachable'})`
-  );
-};
-
-const stopRelay = (proc: ChildProcessWithoutNullStreams): void => {
-  proc.kill();
-};
 
 const evmMine = async (): Promise<unknown> => {
   return provider.send('evm_mine', []);
@@ -649,9 +534,13 @@ const createUserDefinedRequest = (
       };
 };
 
+const deployContract = <T>(contract: string) => {
+  return ethers
+    .getContractFactory(contract)
+    .then((contractFactory) => contractFactory.deploy() as T);
+};
+
 export {
-  startRelay,
-  stopRelay,
   evmMine,
   evmMineMany,
   increaseBlockchainTime,
@@ -670,10 +559,10 @@ export {
   getSuffixDataAndSignature,
   createSupportedSmartWallet,
   RSK_URL,
+  deployContract,
 };
 
 export type {
-  StartRelayParams,
   CreateSmartWalletParams,
   PrepareRelayTransactionParams,
   SupportedSmartWallet,
